35cf986fbd57968c630906d218a9413a4bf2067b
Chad Kreutzer <kanthalion@gmail.com>
2016-11-11 11:27:07 +0000

    tagging done

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,12 @@
 class TagsController < ApplicationController
   
+  def index
+    session[:return_to] ||= request.referer
+    @tags = Tag.all
+  end
+  
   def show
+    session[:return_to] ||= request.referer
     @tag = Tag.find(params[:id])
   end
 end
